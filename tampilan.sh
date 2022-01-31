6078d72ccad3f358a8256b41a2baf5d7664c93db
Creator Lord-Botz <98245473+Lord-Ammar@users.noreply.github.com>
2022-01-31 13:03:17 +0700

    Update tampilan.sh

--- tampilan.sh
+++ tampilan.sh
@@ -2,15 +2,16 @@
 clear
 echo "Starting......."
 sleep 8
-echo "Hapus Semua Chat.."
-sleep 3
+echo "Hapus Semua Text.."
+sleep 15
 cd /data/data/com.termux/files/home
 nano ../usr/etc/motd
 echo "Tampal File Yang Sudah Di Download"
-sleep 5
+sleep 15
 cd tampilan
 cd /data/data/com.termux/files/home
 nano .bashrc
+cd tampilan
 echo "finished changing the display termux"
 echo "Restarting Your termux...."
 sleep 5
